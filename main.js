3ead8361016c524b1038461eafbca0be054a72c9
meeba-nc <mikhailbaum@icloud.com>
2020-12-17 04:53:11 +0400

    making connection between redis and heroku 22

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ if (process.env.VK_TOKEN) {
 
     var rtg = require("url").parse(process.env.REDIS_URL);
     var redis = require("redis").createClient(rtg.port, rtg.hostname);
-    redis.auth(rtg.auth.split(":")[1]);
+    redis.auth(rtg.auth, (err, result) => console.log( "redis: ", err, result ));
 
     bot = new Botact({
         token: process.env.VK_TOKEN,
