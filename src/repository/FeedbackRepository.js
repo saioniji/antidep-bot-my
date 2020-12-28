9a176a966592c653e86fb344421efa4c650618cd
meeba-nc <mikhailbaum@icloud.com>
2020-12-29 03:24:06 +0400

    fixed feedback command 3

--- src/repository/FeedbackRepository.js
+++ src/repository/FeedbackRepository.js
@@ -2,7 +2,8 @@ const Feedback = require('../models/Feedback');
 const { formatDate } = require('../external');
 
 function addFeedback(vk_id, text) {
-    Feedback.save({ vk_id: vk_id, text: text, date: formatDate() }, function(err) {
+    var feedback = new Feedback({ vk_id: vk_id, text: text, date: formatDate() });
+    feedback.save( function(err) {
         if (err) { console.log(err); }
         else console.log('feedback was saved');
     });
